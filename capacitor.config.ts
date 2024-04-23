ed4d060fa8b60b509265b01f2a915bce49381820
Luca Reber <luca.reber@students.fhnw.ch>
2024-04-23 09:53:34 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mobilegi_2024',
+  webDir: 'www'
+};
+
+export default config;
